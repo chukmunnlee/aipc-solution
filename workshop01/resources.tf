c1933e311f59b98622e55aa9fd8346abdf738de4
Chuk <chukmunnlee@gmail.com>
2021-06-21 16:08:31 +0800

    ADDED: workshop01 - configure nginx

--- workshop01/resources.tf
+++ workshop01/resources.tf
@@ -35,7 +35,30 @@ resource digitalocean_droplet nginx {
   size = var.DO_size
   region = var.DO_region
   ssh_keys = [ digitalocean_ssh_key.default-key.fingerprint ]
-  user_data = file("./nginx-config.yaml")
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = file("./keys/mykey")
+    host = self.ipv4_address
+  }
+
+  provisioner remote-exec {
+    inline = [
+      "/usr/bin/apt update -y",
+      "/usr/bin/apt upgrade -y",
+      "/usr/bin/apt install nginx -y",
+    ]
+  }
+  provisioner file {
+    source = "./${local_file.nginx_conf.filename}"
+    destination = "/etc/nginx/nginx.conf"
+  }
+  provisioner remote-exec {
+    inline = [
+      "/usr/sbin/nginx -s reload"
+    ]
+  }
 }
 
 resource local_file root_at_ip {
